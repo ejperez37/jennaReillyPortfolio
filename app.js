218b8e8d7c69a4aef1bf4b50825a0a1b042b507e
E.J. Perez <ejperez37@gmail.com>
2023-04-19 13:03:32 -0400

    Set up other html files and other css stylesheets

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const workCards = document.querySelectorAll('.work-card');
+workCards.forEach((card) => {
+    card.addEventListener('click', () => {
+      const slider = tns({
+        container: '.grid-container',
+        items: 1,
+        slideBy: 1,
+        autoplay: false,
+        controls: true,
+        nav: false,
+        mouseDrag: true,
+        arrowKeys: true,
+      });
+    });
+  });
